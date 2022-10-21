b9fdfdd573dab33bb65aca73f884198dd0ae1824
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-10-21 19:27:47 +0300

    _v5b

---
+++ pages/success_payment.js
@@ -0,0 +1,17 @@
+import Navigation from "../components/navigation";
+import Footer from "../components/footer";
+
+import React from "react";
+
+export default function errorPage() {
+  return (
+    <>
+      <Navigation />
+      <div className="container">
+      <p>Оплата успiшна</p>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
