37cec7a2dc8e085d4d392ae616c480cc524e7b47
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-02-15 13:54:59 +0200

    _v7d

--- next.config.js
+++ next.config.js
@@ -7,11 +7,11 @@ const nextConfig = {
 module.exports = nextConfig;
 
 module.exports = {
-  // experimental: {
-  //   images: {
-  //     unoptimized: true,
-  //   },
-  // },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
 };
 
 module.exports = {
